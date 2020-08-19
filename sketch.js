b141e53228a705f2ce674b0de929ced4e2b7c4e0
Roopal-demo <63741128+Roopal-demo@users.noreply.github.com>
2020-08-19 21:00:31 +0530

    Add files via upload

---
+++ sketch.js
@@ -0,0 +1,43 @@
+
+const Engine = Matter.Engine;
+const World = Matter.World;
+const Bodies = Matter.Bodies;
+const Body = Matter.Body;
+var paperBall,ground,dustbin;
+
+function preload()
+{
+	
+}
+
+function setup() {
+	//createCanvas(800, 700);
+  createCanvas(1600, 700);
+	rectMode(CENTER);
+
+	engine = Engine.create();
+	world = engine.world;
+
+	//Create the Bodies Here.
+    //paperBall = new Ball (50,780,20);
+    //ground = new Ground (350,790,700,20);
+    ground = new Ground(width/2,670,width,20)
+    dustbin=new Dustbin(1200,650);
+    //dustbin = new Dustbin;
+	Engine.run(engine);
+  
+}
+
+
+function draw() {
+  rectMode(CENTER);
+  background(0);
+  //paperBall.display();
+  ground.display();
+  dustbin.display();
+  //drawSprites();
+ 
+}
+
+
+
